5e9ed8f623fca2231eca8f0040ac9a81f7bf78f4
Kevin Peterson <kevin.peterson@mayo.edu>
2013-08-19 13:51:12 -0500

    Updated header.

--- LexEVSAnalyiticalService/src/org/LexGrid/LexBIG/cagrid/LexEVSGridService/CodedNodeGraph/client/CodedNodeGraphClientBase.java
+++ LexEVSAnalyiticalService/src/org/LexGrid/LexBIG/cagrid/LexEVSGridService/CodedNodeGraph/client/CodedNodeGraphClientBase.java
@@ -1,3 +1,11 @@
+/*
+* Copyright: (c) Mayo Foundation for Medical Education and
+* Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+* triple-shield Mayo logo are trademarks and service marks of MFMER.
+*
+* Distributed under the OSI-approved BSD 3-Clause License.
+* See http://ncip.github.com/lexevs-grid/LICENSE.txt for details.
+*/
 package org.LexGrid.LexBIG.cagrid.LexEVSGridService.CodedNodeGraph.client;
 
 import java.io.InputStream;
